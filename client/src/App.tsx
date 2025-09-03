397e6fbd4566ffe364e5c4b436b18db7f8595c8e
Fabricio Guacuto <guacutofabricio@gmail.com>
2025-09-02 21:18:30 -0700

    feat: Initializes the react + ts client with tailwindcss v4

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="text-3xl text-blue-600 font-bold underline text-center mt-10">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
